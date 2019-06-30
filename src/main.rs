84564d6be64d52f89ddeb3d92536edc83485ee4e
ROki1988 <roki.swindler@gmail.com>
2019-06-30 22:25:35 +0900

    use unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 use core::iter::Iterator;
-use std::{env, io};
+use csv::{ReaderBuilder, StringRecord};
 use std::collections::HashMap;
 use std::io::Write;
 use std::path::Path;
-use csv::{ReaderBuilder, StringRecord};
+use std::{env, io};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path = Path::new(&args[1]);
-    let mut rdr = ReaderBuilder::new()
-        .from_path(path)
-        .unwrap();
+    let mut rdr = ReaderBuilder::new().from_path(path).unwrap();
 
     let h = rdr.headers().unwrap().clone();
 
@@ -18,8 +16,10 @@ fn main() {
 
     let stdout = io::stdout();
     let mut locked = stdout.lock();
-    let ss= rs.map(|record: StringRecord| {
-        let vs = record.iter().map(|v| serde_json::from_str(v)).flatten();
+    let ss = rs.map(|record: StringRecord| {
+        let vs = record.iter().map(|v| {
+            serde_json::from_str(v).unwrap_or_else(|_| serde_json::Value::String(v.to_owned()))
+        });
         let out: HashMap<&str, serde_json::Value> = h.iter().zip(vs).collect();
         let mut s = serde_json::to_string(&out).unwrap();
         s.push('\n');
@@ -27,8 +27,6 @@ fn main() {
     });
 
     ss.for_each(|s| {
-        locked
-            .write(s.as_bytes())
-            .unwrap();
+        locked.write(s.as_bytes()).unwrap();
     })
 }
